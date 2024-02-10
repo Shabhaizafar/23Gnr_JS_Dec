a980bff1c90e2ba9caf3fed8dc313e2243fdba00
Shabhai Zafar <shabhaizafar@gmail.com>
2024-02-10 16:54:10 +0530

    Proto,Prototype

---
+++ Feb 07_10/createFu5.js
@@ -0,0 +1,40 @@
+//create a Multiple object using Function
+function User(fname,lname,age,email,address){
+    // 
+    var user = Object.create(User.prototype);
+    user.fname = fname;
+    user.lname = lname;
+    user.age = age;
+    user.email = email;
+    user.address = address;
+    return user;
+};
+User.prototype =  {
+    about : function (){
+        console.log( `My Name is ${this.fname} ${this.lname}.I am ${this.age} years old.`);
+    },
+    is18 : function(){
+        console.log(this.age>=18);
+    },
+    Hello1(){
+        console.log("Hello 1");
+    },
+    Hello2(){
+        console.log("Hello 2");
+    }
+};
+var user1 = User("Raj","Patel",20,"raj@gmail.com","Gandhinagar");
+var user2 = User("Ajay","Shah",23,"ajay@gmail.com","AMD");
+var user3 = User("vijay","Patel",24,"v@gmail.com","Goa");
+
+console.log(user1);
+// console.log(user2);
+// console.log(user3);
+
+user1.about();
+// user2.about();
+// user3.Hello1();
+
+// console.log(user1.__proto__);
+
+// __proto__ === [[prototype]]
